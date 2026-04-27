1b860ae59237a64f214c979fd3c0bee25f9deeb9
admin <yonex12360@gmail.com>
2026-04-27 09:14:34 +0700

    footer

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import { SiteFooter } from '@/components/site-footer'
+
+export default function AuthLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className='flex min-h-svh flex-col'>
+      <div className='flex-1'>{children}</div>
+      <SiteFooter />
+    </div>
+  )
+}
